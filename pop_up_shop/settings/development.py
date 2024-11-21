9a46dd508ccdb2c590bfec5b84823a24525800b5
Paul <ramoneblack@me.com>
2024-11-21 02:45:01 -0500

    first commit

---
+++ pop_up_shop/settings/development.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from .base import *
+import os
+# from dotenv import load_dotenv
+
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
