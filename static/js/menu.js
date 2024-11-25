6f46c5e70b4b341180d96e07642f23a400525077
Paul <ramoneblack@me.com>
2024-11-24 22:45:34 -0500

    secondCommit

--- static/js/menu.js
+++ static/js/menu.js
@@ -4,11 +4,19 @@ const toggle = body.querySelector('.toggle')
 const searchBtn = body.querySelector('.search-box')
 const modeSwitch = body.querySelector('.toggle-switch')
 const modeText = body.querySelector('.mode-text')
+const navLogoText = body.querySelector('.logo-text')
 
 
 toggle.addEventListener('click', () => {
     sidebar.classList.toggle('close');
-})
+    navLogoText.classList.toggle('abbrev')
+    if (navLogoText.classList.contains('abbrev')) {
+        navLogoText.innerHTML = "TPU"
+    } else {
+        navLogoText.innerHTML = "The Pop Up"
+    }
+
+});
 
 searchBtn.addEventListener('click', () => {
     sidebar.classList.remove('close');
