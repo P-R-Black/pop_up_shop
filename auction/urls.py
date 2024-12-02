9957973eb8481f81b91e73a1c1b9a4d62e180963
Paul <ramoneblack@me.com>
2024-12-01 20:52:44 -0500

    bidModal

--- auction/urls.py
+++ auction/urls.py
@@ -5,6 +5,7 @@ app_name = 'auction'
 urlpatterns = [
     path('', views.all_auction_view, name="auction"),
     path('/open', views.product_auction_view, name='product_auction'), # map to product id
+
     
 
 ]
--- static/js/menu.js
+++ static/js/menu.js
@@ -31,3 +31,30 @@ modeSwitch.addEventListener('click', () => {
         modeText.innerText = "Dark Mode"
     }
 })
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the modal, button, and close span
+    const modal = document.getElementById('bidModal');
+    const btn = document.getElementById('bidButton');
+    const span = document.querySelector('.closeModal');
+    console.log('span', span)
+
+    // Show the modal when the button is clicked
+    btn.addEventListener('click', function () {
+        modal.style.display = 'block';
+    });
+
+    // Close the modal when the close span is clicked
+    span.addEventListener('click', function () {
+        modal.style.display = 'none';
+    });
+
+    // Close the modal when clicking outside of it
+    window.addEventListener('click', function (event) {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+});
