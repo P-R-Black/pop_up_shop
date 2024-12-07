984295c9b92aaf1250a0d6b147b9e857bb145a71
Paul <ramoneblack@me.com>
2024-12-07 17:03:54 -0500

    bidModalStyling

--- auction/views.py
+++ auction/views.py
@@ -19,26 +19,10 @@ def product_auction_view(request):
     # need days left in auction
     return render(request, 'auction/product_auction.html')
 
-
-# def product_bid(request, product_id):
-def product_bid(request, product_id=22):
-    print('product_bid called!')
-    return render(request, 'auction/prodcut_bid.html')
-    # if request.method == "POST":
-    #     # bid_amount = request.POST.get('bid_amount')
-    #     bid_amount: 333
-        
-    #     product = "a product" #= get_object_or_404(Product, id=product_id)
-
-    #     try:
-    #         bid_amount = float(bid_amount)
-    #         if bid_amount < product.minimum_bid:
-    #             return JsonResponse({'success': False, 'message': 'Bid amount is too low.'})
-
-    #         # Save the bid
-    #         # Bid.objects.create(user=request.user, product=product, amount=bid_amount)
-    #         return JsonResponse({'success': True, 'message': 'Your bid has been submitted.'})
-    #     except ValueError:
-    #         return JsonResponse({'success': False, 'message': 'Invalid bid amount.'})
-    
-    # return JsonResponse({'success': False, 'message': 'Invalid request'})
+def product_buy_view(request):
+    print('page requested')
+    # need payment option attached to account  | # shopping cart length
+    # shopping cart amount  |  # state tax info  | # ship to info  |  # product pic 
+    # product title  |  # product size  |  # product buy now price  |  # quantity
+    # estimated delivery info  |  # delivery address
+    return render(request, 'auction/product_buy.html')
