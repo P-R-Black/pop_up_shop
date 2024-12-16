61f945d4c21684f5ea5a629e857d51ff0a1468e6
Paul <ramoneblack@me.com>
2024-12-15 21:09:46 -0500

    addressChangeModal

--- static/js/menu.js
+++ static/js/menu.js
@@ -33,13 +33,12 @@ modeSwitch.addEventListener('click', () => {
 })
 
 
-
+// bid modal
 document.addEventListener('DOMContentLoaded', function () {
     // Get the modal, button, and close span
     const modal = document.getElementById('bidModal');
     const btn = document.getElementById('bidButton');
     const span = document.querySelector('.closeModal');
-    console.log('span', span)
 
     // Show the modal when the button is clicked
     btn.addEventListener('click', function () {
@@ -57,4 +56,54 @@ document.addEventListener('DOMContentLoaded', function () {
             modal.style.display = 'none';
         }
     });
+});
+
+
+// address change modal
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the modal, button, and close span
+    const addressChangeModal = document.getElementById('addressChangeModal');
+    const addressChangeBtn = document.getElementById('addressChangeButton');
+    const addressChangeSpan = document.querySelector('.closeAddressModal');
+
+    // Show the modal when the button is clicked
+    addressChangeBtn.addEventListener('click', function () {
+        addressChangeModal.style.display = 'block';
+    });
+
+    // Close the modal when the close span is clicked
+    addressChangeSpan.addEventListener('click', function () {
+        addressChangeModal.style.display = 'none';
+    });
+
+    // Close the modal when clicking outside of it
+    window.addEventListener('click', function (event) {
+        if (event.target === addressChangeModal) {
+            addressChangeModal.style.display = 'none';
+        }
+    });
+});
+
+
+
+
+var expandButton = document.querySelector(".expandButton");
+var checkoutOptionsContainer = document.querySelector('.checkout_options_container')
+console.log('expandButton', expandButton)
+console.log('checkoutOptionsContainer', checkoutOptionsContainer)
+
+expandButton.addEventListener("click", function () {
+    console.log('button clicked')
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+    checkoutOptionsContainer.classList.toggle('show')
+
+    /* Toggle between hiding and showing the active panel */
+    // var panel = this.nextElementSibling;
+    // if (panel.style.display === "block") {
+    //     panel.style.display = "none";
+    // } else {
+    //     panel.style.display = "block";
+    // }
 });
