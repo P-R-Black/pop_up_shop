ceeb462873d395c7c1cea7de46a9836ba94e8a73
Paul <ramoneblack@me.com>
2025-01-19 22:07:33 -0500

    dashboardPersonalData

--- pop_accounts/urls.py
+++ pop_accounts/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'pop_accounts'
 urlpatterns = [
     path('dashboard', views.dashboard, name='dashboard'),
+    path('personal-information', views.personal_info, name='personal_info')
 ]
--- pop_accounts/views.py
+++ pop_accounts/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 def dashboard(request):
     return render(request, ('pop_accounts/user_accounts/dashboard.html'))
+
+def personal_info(request):
+    return render(request, 'pop_accounts/user_accounts/personal_info.html')
+
+def interested_in(request):
+    pass
+
+def on_notice(request):
+    pass
+
+def open_bids(request):
+    pass
+
+def past_bids(request):
+    pass
+
+def past_purchases(request):
+    pass
