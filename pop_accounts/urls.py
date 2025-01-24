bcc228ed325bc16464823acb0bbe0cfb08be0224
Paul <ramoneblack@me.com>
2025-01-23 22:35:18 -0500

    userDashboardinterestedInPage

--- pop_accounts/urls.py
+++ pop_accounts/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = 'pop_accounts'
 urlpatterns = [
     path('dashboard', views.dashboard, name='dashboard'),
-    path('personal-information', views.personal_info, name='personal_info')
+    path('personal-information', views.personal_info, name='personal_info'),
+    path('interested-in', views.interested_in, name='interested_in'),
+    path('on-notice', views.on_notice, name='on_notice'),
+    path('open-bids', views.open_bids, name='open_bids'),
+
 ]
