3a9dabcfe91e2e1650492808d26c45d8556289c2
Paul <ramoneblack@me.com>
2025-07-12 22:33:37 -0400

    pastBidProductDetails

--- auction/urls.py
+++ auction/urls.py
@@ -16,7 +16,8 @@ urlpatterns = [
     path('coming-soon/<slug:slug>/', ComingSoonView.as_view(), name='coming_soon'),
     path('future-releases/', FutureReleases.as_view(), name='future_releases'),
     path('future-releases/<slug:slug>/', FutureReleases.as_view(), name='future_releases'),
-    path('<slug:slug>', ProductDetailView.as_view(), name='product_detail'),
-    path('past-product-details/<int:item_id>', views.past_product_detail, name='past_product_details')
+    path('<slug:slug>/', ProductDetailView.as_view(), name='product_detail'),
+    path('past-product-details/<int:item_id>/', views.past_product_detail, name='past_product_details'),
+    path('past-bids-product-detail/<int:product_id>/', views.past_bid_product_detail_by_product, name='past_bids_product_detail')
    
 ]
