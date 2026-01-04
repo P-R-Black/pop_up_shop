1af5b5f5807bad5638799b408e05cc1f47d01790
Paul <ramoneblack@me.com>
2026-01-04 14:10:50 -0500

    Fix Celery import and make CI-safe

--- pop_up_shop/celery.py
+++ pop_up_shop/celery.py
@@ -3,12 +3,25 @@ from celery import Celery
 
 
 # Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pop_up_shop.settings.base')
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    os.getenv(
+        'DJANGO_SETTINGS_MODULE',
+        'pop_up_shop.settings.development'
+    )
+)
+
 app = Celery('pop_up_shop')
 app.config_from_object('django.conf:settings', namespace='CELERY')
-
-# Add this line to use Celery Beat as the periodic task scheduler
-app.conf.beat_scheduler = "django_celery_beat.schedulers.DatabaseScheduler"
-
-
 app.autodiscover_tasks()
+
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pop_up_shop.settings.development')
+# app = Celery('pop_up_shop')
+# app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# # Add this line to use Celery Beat as the periodic task scheduler
+# app.conf.beat_scheduler = "django_celery_beat.schedulers.DatabaseScheduler"
+
+
+# app.autodiscover_tasks()
