06141d4343d025b8eb3ec717566f18722cc67a2a
Paul <ramoneblack@me.com>
2026-01-07 11:10:08 -0500

    Fix merge conflict markers in celery.py

--- pop_up_shop/celery.py
+++ pop_up_shop/celery.py
@@ -3,29 +3,20 @@ from celery import Celery
 
 
 # Set the default Django settings module for the 'celery' program.
-<<<<<<< HEAD
-os.environ.setdefault(
-    'DJANGO_SETTINGS_MODULE',
-    os.getenv(
-        'DJANGO_SETTINGS_MODULE',
-        'pop_up_shop.settings.development'
-    )
-)
-
-=======
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pop_up_shop.settings.development')
->>>>>>> 5ecd5c2ae04902932d1dc4b0f20cadeb64f1619b
-app = Celery('pop_up_shop')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
 
 
 # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pop_up_shop.settings.development')
 # app = Celery('pop_up_shop')
 # app.config_from_object('django.conf:settings', namespace='CELERY')
-
-# # Add this line to use Celery Beat as the periodic task scheduler
-# app.conf.beat_scheduler = "django_celery_beat.schedulers.DatabaseScheduler"
-
-
 # app.autodiscover_tasks()
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pop_up_shop.settings.development')
+app = Celery('pop_up_shop')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Add this line to use Celery Beat as the periodic task scheduler
+app.conf.beat_scheduler = "django_celery_beat.schedulers.DatabaseScheduler"
+
+
+app.autodiscover_tasks()
