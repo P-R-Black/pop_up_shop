4ddece3bd63e88cc1d56147e3b6babfab87b440b
Paul <ramoneblack@me.com>
2026-01-10 09:59:23 -0500

    Fix 3 pop_up_auction/migrations/0001_initial.py

--- pop_up_auction/migrations/0001_initial.py
+++ pop_up_auction/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('accounts', '0002_user_middle_name'),,
+        ('accounts', '0002_user_middle_name'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
